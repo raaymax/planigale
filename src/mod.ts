7ccd38786b50fefeb52e71231e6af6b41e65d023
Mateusz Russak <mateusz@russak.biz>
2024-07-06 10:42:21 +0200

    fix: qs import failing

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,5 @@
-export * from './errors.ts';
+export * from './app.ts';
 export * from './route.ts';
 export * from './req.ts';
 export * from './res.ts';
-export * from './app.ts';
+export * from './errors.ts';
--- src/req.ts
+++ src/req.ts
@@ -1,4 +1,4 @@
-import { qs } from 'qs';
+import qs from 'qs';
 
 export type ReqInit = {
 	ip: Req['ip'],
